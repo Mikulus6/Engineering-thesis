3dc5a56ea1c8811642372c9afb0718f24e519f7f
Mikulus6 <mikulus6@gmail.com>
2025-04-08 11:02:19 +0200

    fixes + refactors + added solutions averaging

--- DFWM/far_detuned_fwm.py
+++ DFWM/far_detuned_fwm.py
@@ -1,24 +1,20 @@
-
 import os
-
 import numpy as np
-import matplotlib.pyplot as plt
-
 import gnlse
-
+from DFWM.far_detuned_fwm_plots import plot_solution
 
 if __name__ == '__main__':
     setup = gnlse.GNLSESetup()
 
     # Numerical parameters
     setup.resolution = 2**14
-    setup.time_window = 20  # ps
+    setup.time_window = 10  # ps
 
     setup.z_saves = 51
 
     # Physical parameters
-    setup.wavelength = 10004  # nm
-    setup.fiber_length = 0.1  # m
+    setup.wavelength = 1064  # nm
+    setup.fiber_length = 0.2  # m
     setup.raman_model = None #gnlse.raman_blowwood
     setup.self_steepening = True
 
@@ -55,7 +51,7 @@ if __name__ == '__main__':
 
     # This example extends the original code with additional simulations for
     setup.pulse_model = gnlse.CWEnvelope(peak_power, Pn=1e-16)
-    # setup.pulse_model = gnlse.GaussianEnvelope(peak_power, 0.1)
+    # setup.pulse_model = gnlse.GaussianEnvelope(peak_power, 1)
     # setup.pulse_model = gnlse.SechEnvelope(peak_power, 4)
 
     print('%s...' % setup.pulse_model.name)
@@ -63,70 +59,7 @@ if __name__ == '__main__':
     solver = gnlse.GNLSE(setup)
     solution = solver.run()
 
-    plt.figure(figsize=(8, 4), facecolor='w', edgecolor='k')
-    plt.subplot(1, 2, 1)
-    gnlse.plot_wavelength_vs_distance(
-        solution,
-        WL_range=[300, 4000],
-        cmap = 'jet')
-
-    plt.subplot(1, 2, 2)
-    gnlse.plot_delay_vs_distance(
-        solution,
-        time_range=[-10, 10],
-        cmap = 'jet')
-
-    plt.tight_layout()
-    plt.show()
-
-    plt.figure(figsize=(8, 4), facecolor='w', edgecolor='k')
-    plt.subplot(1, 2, 1)
-    gnlse.plot_wavelength_vs_distance(
-        solution,
-        WL_range=[1000, 1100],
-        cmap = 'jet')
-
-    plt.subplot(1, 2, 2)
-    gnlse.plot_delay_vs_distance(
-        solution,
-        time_range=[-5, 5],
-        cmap = 'jet')
-
-    plt.tight_layout()
-    plt.show()
-
-
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
-    gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution,
-        WL_range=[3400, 3700],
-        ax = ax,
-        norm = 51e3)
-    ax.set_ylim([-100, 5])
-
-
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
-    gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution,
-        WL_range=[3400, 3700],
-        ax = ax,
-        z_slice = [0., 0.01, 0.05, .10],
-        norm = 51e3)
-    ax.set_ylim([-400, 5])
-    ax.grid(True)
-
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
-    gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution,
-        WL_range=[620, 640],
-        ax = ax,
-        z_slice = [0., 0.01, 0.05, .10],
-        norm = 51e3)
-    ax.set_ylim([-400, 5])
-    ax.grid(True)
+    plot_solution(solution)
 
     path = "far_detuned_fwm" + \
         "_resolution_" + str(int(np.log(setup.resolution)/np.log(2))) + \
--- DFWM/far_detuned_fwm_plots.py
+++ DFWM/far_detuned_fwm_plots.py
@@ -2,88 +2,88 @@ import matplotlib.pyplot as plt
 import gnlse
 
 
-if __name__ == "__main__":
-    path = "far_detuned_fwm" + \
-        "_resolution_14" + \
-        "_time_window_20" + \
-        "_fiber_length_0.1" + \
-        ".json"
-
-    solution = gnlse.Solution()
-    solution.from_file(path)
-
+def plot_solution(solution_):
     plt.figure(figsize=(8, 4), facecolor='w', edgecolor='k')
     plt.subplot(1, 2, 1)
-    gnlse.plot_wavelength_vs_distance(
-        solution, 
+    gnlse.plot_wavelength_vs_distance_logarithmic(
+        solution_,
         WL_range=[300, 4000],
         cmap = 'jet')
 
     plt.subplot(1, 2, 2)
-    gnlse.plot_delay_vs_distance(
-        solution, 
+    gnlse.plot_delay_vs_distance_logarithmic(
+        solution_,
         time_range=[-40, 40],
         cmap = 'jet')
 
     plt.tight_layout()
     plt.show()
-    
+
     plt.figure(figsize=(8, 4), facecolor='w', edgecolor='k')
     plt.subplot(1, 2, 1)
-    gnlse.plot_wavelength_vs_distance(
-        solution, 
+    gnlse.plot_wavelength_vs_distance_logarithmic(
+        solution_,
         WL_range=[1000, 1100],
         cmap = 'jet')
 
     plt.subplot(1, 2, 2)
-    gnlse.plot_delay_vs_distance(
-        solution, 
+    gnlse.plot_delay_vs_distance_logarithmic(
+        solution_,
         time_range=[-40, 40],
         cmap = 'jet')
 
     plt.tight_layout()
     plt.show()
-    
-    
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
+
+    _, ax = plt.subplots()
     gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution, 
+        solution_,
         WL_range=[3400, 3700],
         ax = ax,
         norm = 51e3)
     ax.set_ylim([-100, 5])
-    
-    
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
+    ax.grid(True)
+
+    _, ax = plt.subplots()
     gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution, 
+        solution_,
         WL_range=[3400, 3700],
         ax = ax,
-        z_slice = solution.Z[0::10],
+        z_slice = solution_.Z[0::10],
         norm = 51e3)
     ax.set_ylim([-200, 50])
     ax.grid(True)
-    
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
+
+    _, ax = plt.subplots()
     gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution, 
+        solution_,
         WL_range=[620, 640],
         ax = ax,
-        z_slice = solution.Z[0::10],
+        z_slice = solution_.Z[0::10],
         norm = 51e3)
     ax.set_ylim([-200, 50])
     ax.grid(True)
-    
-    plt.figure(2, figsize=(8,4), dpi=300)
-    fig2, ax = plt.subplots()
+
+    _, ax = plt.subplots()
     gnlse.plot_wavelength_for_distance_slice_logarithmic(
-        solution, 
+        solution_,
         WL_range=[1050, 1080],
         ax = ax,
-        z_slice = solution.Z[0::10],
+        z_slice = solution_.Z[0::10],
         norm = 51e3)
     ax.set_ylim([-200, 50])
     ax.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    path = "far_detuned_fwm" + \
+        "_resolution_14" + \
+        "_time_window_10" + \
+        "_fiber_length_0.3" + \
+        "_samples_200" + \
+        ".json"
+
+    solution = gnlse.Solution()
+    solution.from_file(path)
+    plot_solution(solution)
