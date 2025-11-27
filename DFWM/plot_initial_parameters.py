f4feb6450834e9b92e75520c95fbbbd22900d354
Mikulus6 <mikulus6@gmail.com>
2025-11-27 11:16:06 +0100

    plot init params visual fixes

--- DFWM/plot_initial_parameters.py
+++ DFWM/plot_initial_parameters.py
@@ -1,27 +1,36 @@
 import os
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import gnlse
 
 def plot_initial_parameters():
 
+    mpl.rcParams['font.family'] = 'Times New Roman'
+    mpl.rcParams['font.size'] = 12
+
+    mpl.rcParams['figure.constrained_layout.use'] = True
+    mpl.rcParams['figure.autolayout'] = True
+
     json_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'neff_far_detuned_FWM.json')
     json_data = gnlse.read_json(json_path)
 
-    lambdas = json_data['neff'][:, 0] * 1e9  # wavelengths in nm
-    neff = json_data['neff'][:, 1]           # effective refractive indices
-    aeff = json_data['neff'][:, 2] * 1e-12   # effective mode area in m^2
+    lambdas = json_data['neff'][:, 0] * 1e9
+    neff = json_data['neff'][:, 1]
+    aeff = json_data['neff'][:, 2] * 1e-2 # to plot easily (correct scalling is 1e-12)
 
     plt.figure()
-    plt.plot(lambdas, neff)
-    plt.xlabel("Wavelength (nm)")
-    plt.ylabel("Effective Refractive Index (n_eff)")
+    plt.plot(lambdas, neff, color="#0080FF")
+    plt.xlabel("Długość fali [nm]")
+    plt.ylabel("Efektywny współczynnik załamania")
     plt.grid(True)
+    plt.tight_layout()
 
     plt.figure()
-    plt.plot(lambdas, aeff)
-    plt.xlabel("Wavelength (nm)")
-    plt.ylabel("Effective Mode Area (m^2)")
+    plt.plot(lambdas, aeff, color="#0080FF")
+    plt.xlabel("Długość fali [nm]")
+    plt.ylabel("Efektywne pole modu [m² · 10⁻¹⁰]")
     plt.grid(True)
+    plt.tight_layout()
 
     plt.show()
 
