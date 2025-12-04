two slopes norm + corrected averaging solutions

DFWM/average_solutions.py
@@ -7,9 +7,8 @@ from DFWM.plot_gain import plot_gain
 
 if __name__ == '__main__':
 
-    setup = define_setup(resolution=2**14, time_window=10, z_saves=51, wavelength=1064, fiber_length=0.6,
+    setup = define_setup(resolution=2**14, time_window=10, z_saves=51, wavelength=1064, fiber_length=0.15,
                          raman_model=None, envelope="CW", rtol=None, atol=None)
-
     samples = 10
     solutions = []
 
@@ -22,8 +21,8 @@ if __name__ == '__main__':
                                       W=  np.mean([sol.W for sol in solutions],          axis=0),
                                       w_0=np.mean([sol.w_0 for sol in solutions],        axis=0),
                                       Z=  np.mean([sol.Z for sol in solutions],          axis=0),
-                                      At= np.mean([np.abs(np.sqrt(sol.At)) for sol in solutions], axis=0)**2,
-                                      AW= np.mean([np.abs(np.sqrt(sol.AW)) for sol in solutions], axis=0)**2)
+                                      At= np.sqrt(np.mean([np.abs(sol.At)**2 for sol in solutions], axis=0)),
+                                      AW= np.sqrt(np.mean([np.abs(sol.AW)**2 for sol in solutions], axis=0)))
 
     plot_solution(average_solution)
     plot_gain(average_solution)

gnlse/visualization.py
@@ -5,6 +5,7 @@ Various plotting functions for visualizing GNLSE simulations using Matplotlib.
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.colors import TwoSlopeNorm
 
 from scipy.interpolate import RectBivariateSpline
 
@@ -275,7 +276,7 @@ def plot_wavelength_for_distance_slice_logarithmic(solver, WL_range=None,
         for x in range(lIW.shape[1]):
             for y in range(lIW.shape[0]):
                 try:
-                    lIW2[y, x] = max((lIW[y+1, x] - lIW[y, x])/(max(solver.Z)/len(solver.Z)), 0)
+                    lIW2[y, x] = (lIW[y+1, x] - lIW[y, x])/(max(solver.Z)/len(solver.Z))
                 except IndexError:
                     lIW2[y, x] = lIW2[y-1, x]
 
@@ -608,7 +609,7 @@ def plot_wavelength_vs_distance(solver, WL_range=None, ax=None,
 
 def plot_wavelength_vs_distance_logarithmic(solver, WL_range=None,
                                             ax=None, norm=None, cmap="magma",
-                                            *, plot_gain=False):
+                                            *, plot_gain=False, use_zero_norm: bool = False):
     """Plotting results in logarithmic scale in wavelength domain.
 
     Parameters
@@ -655,7 +656,7 @@ def plot_wavelength_vs_distance_logarithmic(solver, WL_range=None,
         for x in range(lIW.shape[1]):
             for y in range(lIW.shape[0]):
                 try:
-                    lIW2[y, x] = max((lIW[y+1, x] - lIW[y, x])/(max(solver.Z)/len(solver.Z)), 0)
+                    lIW2[y, x] = (lIW[y+1, x] - lIW[y, x])/(max(solver.Z)/len(solver.Z))
                 except IndexError:
                     lIW2[y, x] = lIW2[y-1, x]
 
@@ -665,8 +666,13 @@ def plot_wavelength_vs_distance_logarithmic(solver, WL_range=None,
     newWL = np.linspace(np.min(WL_asc), np.max(WL_asc), lIW.shape[1])
     toshow = interpolator(solver.Z, newWL)
 
-    ax.imshow(toshow, origin='lower', aspect='auto', cmap=cmap,
-              extent=[np.min(WL_asc), np.max(WL_asc), 0, np.max(solver.Z)])
+    if not use_zero_norm:
+        ax.imshow(toshow, origin='lower', aspect='auto', cmap=cmap,
+                  extent=[np.min(WL_asc), np.max(WL_asc), 0, np.max(solver.Z)])
+    else:
+        ax.imshow(toshow, origin='lower', aspect='auto', cmap=cmap,
+                  norm = TwoSlopeNorm(vmin=np.min(toshow), vcenter=0, vmax=np.max(toshow)),
+                  extent=[np.min(WL_asc), np.max(WL_asc), 0, np.max(solver.Z)])
     ax.set_xlabel("Wavelength [nm]")
     ax.set_ylabel("Distance [m]")
     return ax